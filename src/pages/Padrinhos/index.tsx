c079c219d8fb1657c4027dbd77434dc043b404de
Bruno Amorim <bruno.amorim@bankeiro.com.br>
2024-02-06 23:39:13 -0300

    Update Padrinhos page with images and styling

--- src/pages/Padrinhos/index.tsx
+++ src/pages/Padrinhos/index.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Header from "../../components/Header";
 import "./styles.scss";
+
+import madrinhas from "../../assets/img/madrinhas.jpeg";
+import padrinho from "../../assets/img/padrinho.jpeg";
 
 const Padrinhos = () => {
   return (
@@ -11,16 +15,89 @@ const Padrinhos = () => {
           <h1>Queridos Padrinhos</h1>
 
           <p>
-            Obrigado por aceitarem o convite. Nosso dia são seria o mesmo sem a
-            presença de vocês.
+            Queridos padrinhos, obrigada por aceitarem nosso convite. Ficamos
+            imensamente felizes por compartilhar esse momento especial, não
+            seria o mesmo sem vocês!
           </p>
 
-          <h2>Para o grande dia</h2>
+          <h3>Para o grande dia</h3>
 
           <p>
-            Chegar com meia hora de antecedência ao local da cerimônia. Não
-            podemos atrasar, ok?
+            É importante que vocês cheguem com 40 minutos de antecedência, não
+            podemos nos atrasar, ok?
           </p>
+
+          <h3>Paletas de cores para você se inspirar</h3>
+
+          {/* corações coloridos */}
+
+          <i
+            className="fas fa-circle fa-8x"
+            style={{
+              color: "#54582f",
+            }}
+          ></i>
+          <i
+            className="fas fa-circle fa-8x"
+            style={{
+              color: "#86895d",
+            }}
+          ></i>
+          <i
+            className="fas fa-circle fa-8x"
+            style={{
+              color: "#bec092",
+            }}
+          ></i>
+          <i
+            className="fas fa-circle fa-8x"
+            style={{
+              color: "#f8fbca",
+            }}
+          ></i>
+
+          <h3
+            style={{
+              marginTop: 100,
+            }}
+          >
+            Madrinha
+          </h3>
+
+          <p>
+            Queremos que nesse dia você se sinta linda e confortável, então o
+            modelo do vestido é de sua preferência contanto que seja longo!
+          </p>
+
+          <img
+            src={madrinhas}
+            style={{
+              width: "22rem",
+              borderRadius: "8px",
+            }}
+          />
+
+          <br />
+          <br />
+
+          <h3>Padrinho</h3>
+
+          <p>
+            Para você, indicamos um terno preto, camisa branca e a gravata
+            também na cor verde oliva, combinando com sua acompanhante.
+          </p>
+
+          <img
+            src={padrinho}
+            style={{
+              width: "22rem",
+              borderRadius: "8px",
+            }}
+          />
+          <br />
+          <br />
+          <br />
+          <br />
         </div>
       </div>
     </>
