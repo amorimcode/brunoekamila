edf48fad2be8205ca814e1b752333dcebff568ea
Bruno Amorim <71152836+amorimcode@users.noreply.github.com>
2024-09-09 00:06:37 -0300

    Update index.tsx

--- src/pages/Presentes/index.tsx
+++ src/pages/Presentes/index.tsx
@@ -50,22 +50,22 @@ const Presentes = () => {
     {
       title: 'Organizador de Geladeira',
       foto: "https://m.media-amazon.com/images/I/51Pvq4I-mIL._AC_SL1200_.jpg",
-      url: 'https://www.amazon.com.br/T%C3%A1buas-Teca-Suporte-Organizador-Stolf/dp/B08VRP737Y/ref=sr_1_7?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=26Y1HYISAODQZ&dib=eyJ2IjoiMSJ9.8nil0ip8ih-1FA3MD4ayhWmFMFxTs6S_Cz4BaGrVtYocOn9j6hOKzGyllhNwQPHrWp1Oq-wjm3CeNtZyCiGej6UYixSMLkXAJGUciqcY-akMXP_S0EDIrTh9K9HMco-EAMHCS3MkUl6zcit6d7p1VwIlAp4dUptixLBEQ-ubpFTO6HHV9DEZw6OBx-H9pYHLtO5oa5Wm1mV83Q5E7_0Cg55UbtPxHKWqXgaplZ8-9p4-W_y8ORQfEQz7BnHcTxY1n6LBWfNYk9LtJ_VA-1LSRRsh3mLAZ8unyziOPtgcBwo.UPDrii6GsGQ6OS0cWnATw-VlFcGHSTuP7lvom28VFec&dib_tag=se&keywords=tabua&qid=1725850701&sprefix=tab%2Caps%2C188&sr=8-7&ufe=app_do%3Aamzn1.fos.6d798eae-cadf-45de-946a-f477d47705b9'
+      url: 'https://www.amazon.com.br/Organizadores-Geladeira-Arm%C3%A1rio-Alimentos-Vegetais/dp/B0C2JML33J/ref=sr_1_1_sspa?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=AVJYIOB5HZG8&dib=eyJ2IjoiMSJ9.COrtzoLKnSQibkZbNHEnUiy-8Ox10RfGqMNqQFaZeTALSyd_ggflQykIRUAhJc-sv3ykILr8ay_dtEcxZqOPlJsZAKLutjGvcJIOcjO6aLhiEWAaghnR3C9APmeD-a5cFQapJLLdWRj47oX_f6asQkdlAZC18M3k4jZlTbaW1KYdXTZxpvcB8Lz9iSGPtSVo9S3iEIigIu4uSWsXQ0Z0Ew9mVhXXcnYWO60GCutUrLcjvWGpXK6V2IbvkGakgI9kwN6UeBMPTUo3lKOZ3NpAUFMklTOYvpWcFUfaoJ9icpc.EoqncnFjJ8S8-kpwS71jbb2SY-oqcMM0VK_mOCJWb54&dib_tag=se&keywords=organizador+de+geladeira&qid=1725851039&sprefix=organizador+de+geladei%2Caps%2C189&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1'
     },
     {
       title: 'Pote Hermetico',
       foto: "https://m.media-amazon.com/images/I/519ZH-Fhc1L._AC_SL1000_.jpg",
-      url: 'https://www.amazon.com.br/Kit-Potes-Herm%C3%A9ticos-Pl%C3%A1stico-Electrolux/dp/B09XJL4B9H/ref=asc_df_B09XJL4B9H/?tag=googleshopp00-20&linkCode=df0&hvadid=379725881673&hvpos=&hvnetw=g&hvrand=14598484904077665038&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001729&hvtargid=pla-1662902312395&mcid=93036d57993a35b7af417811b1a3528a&th=1'
+      url: 'https://www.amazon.com.br/Kit-Potes-Herm%C3%A9ticos-Pl%C3%A1stico-Electrolux/dp/B09XJL4B9H/ref=sr_1_5?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3K29RQXDY2KJT&dib=eyJ2IjoiMSJ9.AXSNal2bQPawx3ZbYkw93QTF1bnKnEhqB_oWd-rl3ssa_Eqipo35VIh8XctXl9VMV9E6F6ucKcen8gyj-SGk1zDnPqn8W1YCWj85wAjsISHcaP8sGfKlXUv0hrm7ES7MxI8_w9y7-XXAo3jxaNSZDWkyYd0cc39CYUscHv97EVxRLWv5Rns1FfTr9hu0iugH1xan50yWIRJhxxYkP7S5scBD4O_pt86IqAxo6wt72bdSC_3kdlGH8MMGRlG58EC7ze_UalQy_uhmSbjQp2FeE89jvwALi1BRDKLZ1mxm6Kw.WNb_2Xoc20JOzAlohyFrhUNaTJb-L03NMi0ukFoj3Zk&dib_tag=se&keywords=pote%2Bhermetico&qid=1725850993&sprefix=pote%2Bhermeti%2Caps%2C193&sr=8-5&ufe=app_do%3Aamzn1.fos.6d798eae-cadf-45de-946a-f477d47705b9&th=1'
     },
     {
       title: 'Porta Tempero',
       foto: "https://m.media-amazon.com/images/I/61OG7XRQh5L._AC_SL1200_.jpg",
-      url: 'https://www.amazon.com.br/Porta-Tempero-Condimento-Giratorio-Cozinha/dp/B08L5M5V6H/ref=asc_df_B08L5M5V6H/?tag=googleshopp00-20&linkCode=df0&hvadid=456273449207&hvpos=&hvnetw=g&hvrand=15582696808799468820&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001729&hvtargid=pla-1091134025254&psc=1&mcid=a7349087334b31238947b7723d0bd04e'
+      url: 'https://www.amazon.com.br/Porta-Tempero-Condimento-Giratorio-Cozinha/dp/B08L5M5V6H/ref=sr_1_7?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1KJAVLQT58SIY&dib=eyJ2IjoiMSJ9.NEEf2bXsNuRd1AAE7OZ6HeqQtkKr_yaQ_hEBnZyibTCxhafLoeAm2I10ngR7z4IuhzGh6xrrh5Rs8SyCIXe1TwAmItfgGNTfHJX9UJgb8VucMrkffTaNWDehYI1nifJcf1qUDbMoIiWOQIHk3CRsKce2M4FXTybfiqiDLRw0g0p0KBoupZCQ-EjXNWD1MoyELljxl3RuD3CL5soZR29ExdFyedvpGplX21qL_PY6k4nU4ayAk8K-QqkgQNy-YqTnqwCmc2c08pFx8m98aejSczXro7TlI9mq2fIb22XDPyI.FyhUC3VeJH8HHEKDysiYOclSa1l41pAR_HdDWEHhbms&dib_tag=se&keywords=porta+tempero&qid=1725851107&sprefix=porta+temp%2Caps%2C192&sr=8-7&ufe=app_do%3Aamzn1.fos.a492fd4a-f54d-4e8d-8c31-35e0a04ce61e'
     },
     {
       title: 'Kit travesseiro',
       foto: "https://m.media-amazon.com/images/I/61M5fjeKWpL._AC_SL1200_.jpg",
-      url: 'https://www.amazon.com.br/Kit-Travesseiro-Pe%C3%A7as-Percal-Fios/dp/B076PL3G89/ref=asc_df_B076PL3G89/?tag=googleshopp00-20&linkCode=df0&hvadid=379720772128&hvpos=&hvnetw=g&hvrand=10782962139143374491&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001729&hvtargid=pla-836223934257&psc=1&mcid=f81c6189036939c2846b9f680237c774'
+      url: 'https://www.amazon.com.br/Kit-Travesseiro-Pe%C3%A7as-Percal-Fios/dp/B076PL3G89/ref=sr_1_8?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3MJSWAHKLZ84X&dib=eyJ2IjoiMSJ9.NfB5YYfMeABq-AG_IWfrajpxlPIkJtGYgzX1Yvk2d2_DaTDsbXWrIh_fQbVcjfljZdKYrAokHXcE6_Io3fAdBbGg9v7YbEv8KCof70_7wFttu_-buwJDmmqY1hifRRiX0n4s00B6YhVyE7iBMnS--mKQLFTEnrlRQnvPLPGuZCvT7oUiMA3jtkluMOAQHx3tfdOlQK4WTn265a0PIaEsyjzTUb2cmDAjEoupQN3GILarHJBUji9U7ywJpQEi-SxcxMj3skyEUmU2SSFPIikY7wMcwzB8aWSzTtTfeUcnJ7k.C3TlHPNSsVL_VGSmvR76SYvim-mfeE6OIk78urW1o9E&dib_tag=se&keywords=kit+travesseiro&qid=1725851140&sprefix=kit+travesse%2Caps%2C193&sr=8-8&ufe=app_do%3Aamzn1.fos.a492fd4a-f54d-4e8d-8c31-35e0a04ce61e'
     },
     {
       title: 'Toalhas de banho',
@@ -80,7 +80,7 @@ const Presentes = () => {
     {
       title: 'Tapetes de banheiro',
       foto: "https://m.media-amazon.com/images/I/61afNMVaijL._AC_SL1200_.jpg",
-      url: 'https://www.amazon.com.br/Tapetes-Banheiro-Antiderrapante-Emborrachado-60x40cm/dp/B0CLQ336QR/ref=asc_df_B0CLQ336QR/?tag=googleshopp00-20&linkCode=df0&hvadid=647603346200&hvpos=&hvnetw=g&hvrand=7775287222266336419&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001729&hvtargid=pla-2267084157784&psc=1&mcid=a7e7fa4fbf5437a19c89defb65e31f84'
+      url: 'https://www.amazon.com.br/Tapetes-Banheiro-Antiderrapante-Emborrachado-60x40cm/dp/B0CLQ336QR/ref=sr_1_9?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1MBHB1RN9U6U5&dib=eyJ2IjoiMSJ9.qwojgIc1L4s_4aH40vHgzY_KJCifGovO5eu_97LxcHGpub_cnw-m_9BzAM8zl_S_xLexH8z6TIaifUIJuq4dSXczqQ4RyXdIKDybDGWIWh1P-ykHBae3wg05aUxfFIe11VIWqdYgnhm_Jimyv91UUtelh0HVle49qSmKVxitLPcR0ySsEFAayLfLsNt0A0JuoQawbIKn701LkYWORSxkzY3BL0aDP5GCoi5fyAj_tNPUrJKQF8zJNrVhNIAtgsCHSL314ZxWscJABQ8GxMS395bz75xNw74yKIDdfrg8588.cxWHEW5YxTS5eZPaway0l60TrOT7gB_BlhzVQQ9-8ug&dib_tag=se&keywords=tapete+de+banheiro&qid=1725851176&sprefix=tapetes+de+banhei%2Caps%2C192&sr=8-9'
     },
     {
       title: 'Jarra de vidro',
